e00a082856247c650ed50ae703aef8ebd4dbfa60
Leroy Hermans <l.g.j.hermans@gmail.com>
2018-02-08 13:19:57 +0100

    Added recursive an dnon recursive sum functionality

--- Les2/src/Program.java
+++ Les2/src/Program.java
@@ -1,11 +1,20 @@
 public class Program {
 
     public static void main(String args[]){
+        System.out.println("Faculteit:");
         System.out.println("non recursive:");
         Faculteit.nonRecursiveFaculteit(14);
         System.out.println("");
         System.out.println("RECURSIVE");
         long a = Faculteit.recursiveFaculteit(14);
         System.out.println("Faculteit recursief: " + a);
+
+        System.out.println("SUM:");
+        System.out.println("non recursive:");
+        Som.sumFor(14);
+        System.out.println("");
+        System.out.println("recursive:");
+        int b  = Som.sum(14);
+        System.out.println("Som recursief: " + b);
     }
 }
--- Les2/src/Som.java
+++ Les2/src/Som.java
@@ -1,2 +1,25 @@
 public class Som {
+
+    public static int sum(int n)
+    {
+       if(n == 1)
+           return 1;
+        else
+       return sum(n - 1) + n;
+    }
+
+    public static void sumFor(int n)
+    {
+        StringBuilder stringToPrint = new StringBuilder();
+        int sum = 0;
+        for(int i = 0 ; i <= n; i++)
+        {
+            sum += i;
+            if(i == n)
+                stringToPrint.append(i).append("=");
+            else
+                stringToPrint.append(i).append("+");
+        }
+        System.out.println(stringToPrint + "" + sum);
+    }
 }
