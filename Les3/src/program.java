071bfbee4d2ddebbf449a1646a1fcf26df2d778d
Leroy Hermans <l.g.j.hermans@gmail.com>
2018-02-19 21:06:28 +0100

    Implemented Quicksort algorithm

--- Les3/src/program.java
+++ Les3/src/program.java
@@ -1,15 +1,23 @@
 public class program {
 
     public static void main(String[] args) {
-        int[] arrayToSort = new int[]{3, 1, 14, 6, 18, 45, 20, 66};
-        Mergesort sorter = new Mergesort();
-        sorter.sort(arrayToSort);
-        printArray(arrayToSort);
+        int[] arrayToMergeSort = new int[]{3, 1, 14, 6, 18, 45, 20, 66};
+        Mergesort mergsSorter = new Mergesort();
+        mergsSorter.sort(arrayToMergeSort);
+        printArray(arrayToMergeSort);
 
         System.out.println("");
 
-        int[] insertionSortOutput = Insertionsort.doInsertionSort(arrayToSort);
+        int[] arrayToInsertionSort = new int[]{3, 1, 14, 6, 18, 45, 20, 66};
+        int[] insertionSortOutput = Insertionsort.doInsertionSort(arrayToInsertionSort);
         printArray(insertionSortOutput);
+
+        System.out.println("");
+
+        int[] arrayToQuickSort = new int[]{3, 1, 14, 6, 18, 45, 20, 66};
+        Quicksort quickSorter = new Quicksort();
+        quickSorter.sort(arrayToQuickSort);
+        printArray(arrayToQuickSort);
     }
 
     private static void printArray(int[] arrayToPrint) {
