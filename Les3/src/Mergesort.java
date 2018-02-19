daca7af47cd86939106c5eac8fed9b71f90b9122
Leroy Hermans <l.g.j.hermans@gmail.com>
2018-02-19 20:55:45 +0100

    Implemented insertion sort algorithm + boyscouting

--- Les3/src/Mergesort.java
+++ Les3/src/Mergesort.java
@@ -5,15 +5,15 @@ public class Mergesort {
 
     private int number;
 
-    public void sort(int[] values){
+    public void sort(int[] values) {
         this.numbers = values;
         number = values.length;
         this.helper = new int[number];
         mergesort(0, number - 1);
     }
 
-    private void mergesort(int low, int high){
-        if(low < high){
+    private void mergesort(int low, int high) {
+        if (low < high) {
             int middle = low + (high - low) / 2;
             mergesort(low, middle);
             mergesort(middle + 1, high);
@@ -21,8 +21,8 @@ public class Mergesort {
         }
     }
 
-    private void merge(int low, int middle, int high){
-        for(int i = low; i <= high; i++){
+    private void merge(int low, int middle, int high) {
+        for (int i = low; i <= high; i++) {
             helper[i] = numbers[i];
         }
 
@@ -30,8 +30,8 @@ public class Mergesort {
         int j = middle + 1;
         int k = low;
 
-        while(i <= middle && j <= high){
-            if(helper[i] <= helper[j]){
+        while (i <= middle && j <= high) {
+            if (helper[i] <= helper[j]) {
                 numbers[k] = helper[i];
                 i++;
             } else {
@@ -41,7 +41,7 @@ public class Mergesort {
             k++;
         }
 
-        while(i <= middle){
+        while (i <= middle) {
             numbers[k] = helper[i];
             k++;
             i++;
--- Les3/src/program.java
+++ Les3/src/program.java
@@ -1,14 +1,19 @@
 public class program {
 
     public static void main(String[] args) {
-        int[] arrayToSort = new int[]{3,1,14,6,18,45,20,66};
+        int[] arrayToSort = new int[]{3, 1, 14, 6, 18, 45, 20, 66};
         Mergesort sorter = new Mergesort();
         sorter.sort(arrayToSort);
         printArray(arrayToSort);
+
+        System.out.println("");
+
+        int[] insertionSortOutput = Insertionsort.doInsertionSort(arrayToSort);
+        printArray(insertionSortOutput);
     }
 
-    private static void printArray(int[] arrayToPrint){
-        for(int i = 0; i < arrayToPrint.length; i++){
+    private static void printArray(int[] arrayToPrint) {
+        for (int i = 0; i < arrayToPrint.length; i++) {
             System.out.println(arrayToPrint[i]);
         }
     }
